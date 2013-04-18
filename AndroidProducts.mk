4547e3ba8379a143bdc5df67919cc4a880eb7bf8
im000000 <irumbo@gmail.com>
2013-04-18 09:35:26 -0500

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,7 +1,7 @@
-PRODUCT_MAKEFILES := $(LOCAL_DIR)/full_scorpion_mini.mk
-ifeq ($(TARGET_PRODUCT),aokp_scorpion_mini)
+PRODUCT_MAKEFILES := $(LOCAL_DIR)/full_solstice.mk
+ifeq ($(TARGET_PRODUCT),aokp_solstice)
     PRODUCT_MAKEFILES += $(LOCAL_DIR)/aokp.mk
 endif
-ifeq ($(TARGET_PRODUCT),cna_scorpion_mini)
+ifeq ($(TARGET_PRODUCT),cna_solstice)
     PRODUCT_MAKEFILES += $(LOCAL_DIR)/cna.mk
 endif
