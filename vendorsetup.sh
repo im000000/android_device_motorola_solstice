5f3fdfcd74f06e9a146b30f89d80d4935606e802
im000000 <irumbo@gmail.com>
2013-04-18 09:48:08 -0500

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,4 +1,4 @@
-add_lunch_combo aokp_scorpion_mini-eng
-add_lunch_combo cm_scorpion_mini-userdebug
-add_lunch_combo cna_scorpion_mini-eng
-add_lunch_combo full_scorpion_mini-eng
+add_lunch_combo aokp_solstice-eng
+add_lunch_combo cm_solstice-userdebug
+add_lunch_combo cna_solstice-eng
+add_lunch_combo full_solstice-eng
