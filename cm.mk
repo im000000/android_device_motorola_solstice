ef6721209b9036d9cfabeebda95752fb1e629666
im000000 <irumbo@gmail.com>
2013-04-18 09:38:58 -0500

    Update cm.mk

--- cm.mk
+++ cm.mk
@@ -1,4 +1,4 @@
-$(call inherit-product, device/motorola/scorpion_mini/full_scorpion_mini.mk)
+$(call inherit-product, device/motorola/scorpion_mini/full_solstice.mk)
 
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/cdma.mk)
@@ -9,14 +9,14 @@ $(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
-DEVICE_PACKAGE_OVERLAYS += device/motorola/scorpion_mini/overlay/cm
+DEVICE_PACKAGE_OVERLAYS += device/motorola/solstice/overlay/cm
 
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=scorpion_mini TARGET_DEVICE=scorpion_mini BUILD_FINGERPRINT="motorola/XT907_verizon/scorpion_mini:4.1.1/9.8.1Q_25/35:user/release-keys" PRIVATE_BUILD_DESC="smq_vzw-user 4.1.1 9.8.1Q_25 35 release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=solstice TARGET_DEVICE=solstice BUILD_FINGERPRINT="motorola/XT905_usc/solstice:4.1.1/9.8.1Q_25/35:user/release-keys" PRIVATE_BUILD_DESC="smq_usc-user 4.1.1 9.8.1Q_25 35 release-keys"
 
 # Boot animation
 TARGET_SCREEN_HEIGHT := 960
 TARGET_SCREEN_WIDTH := 540
 
-PRODUCT_NAME := cm_scorpion_mini
-PRODUCT_DEVICE := scorpion_mini
+PRODUCT_NAME := cm_solstice
+PRODUCT_DEVICE := solstice
 
